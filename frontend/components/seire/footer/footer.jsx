1c58f2458c54d2373a8e673386c8e2a0b61a6867
Brekke Green <brekke.andrew.green@gmail.com>
2021-03-23 10:39:21 -0400

    Built out more of the Footer component

--- frontend/components/seire/footer/footer.jsx
+++ frontend/components/seire/footer/footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer id="footer">
-    <div className="footer-div-name">SEIRE</div>
+    <div className="footer-div-name">SEIRE
+        <p>This is a clone of Strava created by Brekke Green and is intended for demonstration purposes only. Route creation was created with the Mapbox API. </p>
+    </div>
     <div className="footer-div-menu">MENU</div>
     <div className="footer-div-follow">FOLLOW</div>
     <div className="footer-div-get-started">GET STARTED</div>
