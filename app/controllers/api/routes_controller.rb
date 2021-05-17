09a0adbf0b0145f481feb74283eea12059e20c3b
Brekke Green <brekke.andrew.green@gmail.com>
2021-05-16 23:49:46 -0400

    Create routes controller

--- app/controllers/api/routes_controller.rb
+++ app/controllers/api/routes_controller.rb
@@ -3,15 +3,27 @@ class Api::RoutesController < ApplicationController
     before_action :require_logged_in, only: [:create, :update, :destroy]
 
     def index 
-        
+        @routes = Route.where(user_id: params[:user_id])
+        render :index
     end
 
     def show
-
+        @routes = Route.find_by(id: params[:user_id])
+        if @routes
+            render :show
+        else
+            render json: ["No route exists."], status: 404
+        end
     end
 
     def create 
-
+        @route = Route.new(route_params)
+        @route.user_id = current_user.id 
+        if @route.save 
+            render :show 
+        else
+            render json: @route.errors.full_messages, status: 401
+        end
     end
     
     def update 
