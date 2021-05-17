d3ed306d5f508b5612df36f2a9b96f61ca37475b
Brekke Green <brekke.andrew.green@gmail.com>
2021-05-17 11:09:57 -0400

    Fix routes model

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,6 +3,6 @@ class Route < ApplicationRecord
     validates :route_data, :user_id, :distance, presence: true
     validates :routeName, uniqueness: { scope: :user_id }
 
-    
-
+    belongs_to :user,
+    foreign_key: :user_id
 end
