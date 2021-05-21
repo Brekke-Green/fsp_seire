2418a39c9d3b5d843a4f5ff991add59f84227efa
Brekke Green <brekke.andrew.green@gmail.com>
2021-05-21 16:44:31 -0400

    Build create workout form and handleSubmit

--- frontend/components/seire/workouts/workout_form.jsx
+++ frontend/components/seire/workouts/workout_form.jsx
@@ -4,7 +4,7 @@ class WorkoutForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+            currentRoute: ""
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -13,8 +13,19 @@ class WorkoutForm extends React.Component {
         this.props.getRoutes();
     }
 
-    handleSubmit() {
+    handleSelection(e) {
+        this.setState({currentRoute: this.props.routes[e.target.value]})
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        const workout = {
+            user_id: this.props.session.id,
+            route_id: e.target.values.route,
+            workout_type: e.target.values.workoutType,
+            duration: (e.target.values.minutes * 60 + e.target.values.seconds)
+        }
+        this.props.createWorkout(workout)
     }
 
     render () {
@@ -26,16 +37,28 @@ class WorkoutForm extends React.Component {
             <div className='main-content workout-form-container'>
                 <div className='workout-form-container'>
                     <form onSubmit={this.handleSubmit} className='workout-form-box'>
-                        <label htmlFor="routes-select">Choose a route:</label>
-                        <select name="routes" id="routes-select">
+                        <label>Workout type:</label>
+                        <select name="workoutType">
+                            <option value="run">Running</option>
+                            <option value="walk">Walking</option>
+                            <option value="hike">Hiking</option>
+                            <option value="cycle">Cycling</option>
+                        </select>
+                        <label>Choose a route:</label>
+                        <select name="route" id="routes-select" onChange={this.handleSelection}>
                             {Object.values(routes).map(route => (
                                 <option key={route.id * 10} value={route.id}>{`${route['route_name']}: ${route['distance']}m`}</option>
                             ))}
                         </select>
-                        <input className="workout-input minutes" type="text" value="Minutes"/>
-                        <input className="workout-input seconds" type="text" value="Seconds"/>
+                        <label>Minutes:</label>
+                        <input className="workout-input minutes" name="minutes" type="text" value="Minutes"/>
+                        <label>Seconds:</label>
+                        <input className="workout-input seconds" name="seconds" type="text" value="Seconds"/>
                         <button>Track Workout</button>
                     </form>
+                    <div className="workout-route-display">
+                        WORKOUT ROUTE DISPLAY
+                    </div>
                 </div>
             </div>
         )
