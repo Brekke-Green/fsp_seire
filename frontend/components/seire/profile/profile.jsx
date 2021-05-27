6b57fa215866107e9062b8f6b29e6f73d5ee75bc
Brekke Green <brekke.andrew.green@gmail.com>
2021-05-27 00:53:15 -0400

    Create user stats framework

--- frontend/components/seire/profile/profile.jsx
+++ frontend/components/seire/profile/profile.jsx
@@ -3,11 +3,31 @@ import React from 'react';
 class Profile extends React.Component {
     constructor(props) {
         super(props);
+        this.calculateMiles = this.calculateMiles.bind(this);
+        this.calculateAverageSpeed = this.calculateAverageSpeed(this);
+    }
+
+    componentDidMount() {
+        this.props.getRoutes();
+        this.props.getWorkouts();
+    }
+
+    calculateMiles() {
+
+    }
+
+    calculateAverageSpeed() {
+        
     }
 
     render() {
+        if (Object.keys(this.props.routes).length < 1 && Object.keys(this.props.workouts).length < 1) { return null; }
         return (
-            <h1>profile/stats section</h1>
+            <div>
+                <div>
+                    THIS WILL BE THE USER STATS
+                </div>
+            </div>
         )
     }
 }
--- frontend/components/seire/profile/profile_container.jsx
+++ frontend/components/seire/profile/profile_container.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { requestRoutes } from '../../../actions/map_actions';
+import { requestWorkouts } from '../../../actions/workout_actions';
 import Profile from './profile';
 
-const mSTP = state => ({
-    
-})
+const mSTP = ({ session, entities, errors }) => {
+    return {
+        workouts: entities.workouts,
+        routes: entities.routes,
+        session: session,
+        errors: errors.workouts,
+    };
+}
 
 const mDTP = dispatch => ({
-
+    getRoutes: () => dispatch(requestRoutes()),
+    getWorkouts: () => dispatch(requestWorkouts())
 })
 
 export default connect(mSTP, mDTP)(Profile);
