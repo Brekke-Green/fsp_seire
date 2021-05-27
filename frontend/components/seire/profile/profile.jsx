4298288832abeb82ce3e4d8d132c53373085333a
Brekke Green <brekke.andrew.green@gmail.com>
2021-05-27 11:52:01 -0400

    Create User stats calculations

--- frontend/components/seire/profile/profile.jsx
+++ frontend/components/seire/profile/profile.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 class Profile extends React.Component {
     constructor(props) {
         super(props);
-        this.calculateMiles = this.calculateMiles.bind(this);
-        this.calculateAverageSpeed = this.calculateAverageSpeed(this);
+        
+        this.userWorkouts = [];
+        this.userRuns = [];
+        this.userWalks = [];
+        this.userHikes = [];
+        this.userCycles = [];
+
+        this.calculateUserMiles = this.calculateUserMiles.bind(this);
+        this.calculateUserAverageSpeed = this.calculateUserAverageSpeed.bind(this);
+        this.filterWorkoutsByType = this.filterWorkoutsByType.bind(this);
+        this.getUserDistance = this.getUserDistance.bind(this);
+        this.getUserDuration = this.getUserDuration.bind(this);
     }
 
     componentDidMount() {
@@ -12,11 +22,39 @@ class Profile extends React.Component {
         this.props.getWorkouts();
     }
 
-    calculateMiles() {
+    getUserWorkouts(userId) {
+        this.userWorkouts = Object.values(this.props.workouts).filter( workout => {return workout.user_id === userId})
+    }
+
+    filterWorkoutsByType(workoutType) {
+        const typeWorkout = this.userWorkouts.filter((workout) => {return workout.workout_type === workoutType});
+        switch (workoutType) {
+            case ("run"):
+                this.userRuns = typeWorkout;
+                break;
+            case ("walk"):
+                this.userWalks = typeWorkout;
+                break;
+            case ("hike"):
+                this.userHikes = typeWorkout;
+                break;
+            case ("cycle"):
+                this.userCycles = typeWorkout;
+                break;
+            default:
+                break;
+        }
+    }
+
+    getUserDurations(type) {
+        
+    }
+
+    calculateUserMiles() {
 
     }
 
-    calculateAverageSpeed() {
+    calculateUserAverageSpeed() {
         
     }
 
