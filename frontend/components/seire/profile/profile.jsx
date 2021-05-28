dfac108c9073d7205536e7ea3be337ed92093880
Brekke Green <brekke.andrew.green@gmail.com>
2021-05-28 14:03:30 -0400

    Create detailed user profile stats calculations

--- frontend/components/seire/profile/profile.jsx
+++ frontend/components/seire/profile/profile.jsx
@@ -3,18 +3,23 @@ import React from 'react';
 class Profile extends React.Component {
     constructor(props) {
         super(props);
+
+        this.workoutTypes = ['run', 'walk', 'hike', 'cycle'];
         
         this.userWorkouts = [];
-        this.userRuns = [];
-        this.userWalks = [];
-        this.userHikes = [];
-        this.userCycles = [];
+        this.userRuns = {};
+        this.userWalks = {};
+        this.userHikes = {};
+        this.userCycles = {};
 
-        this.calculateUserMiles = this.calculateUserMiles.bind(this);
-        this.calculateUserAverageSpeed = this.calculateUserAverageSpeed.bind(this);
+        this.userStats = {};
+
+        this.filterUserWorkouts = this.filterUserWorkouts.bind(this);
         this.filterWorkoutsByType = this.filterWorkoutsByType.bind(this);
-        this.getUserDistance = this.getUserDistance.bind(this);
-        this.getUserDuration = this.getUserDuration.bind(this);
+        this.totalDuration = this.totalDuration.bind(this);
+        this.totalDistance = this.totalDistance.bind(this);
+        this.calculateUserAverageSpeed = this.calculateUserAverageSpeed.bind(this);
+        this.calculateStats = this.calculateStats.bind(this)
     }
 
     componentDidMount() {
@@ -22,48 +27,80 @@ class Profile extends React.Component {
         this.props.getWorkouts();
     }
 
-    getUserWorkouts(userId) {
-        this.userWorkouts = Object.values(this.props.workouts).filter( workout => {return workout.user_id === userId})
+    filterUserWorkouts(userId) {
+        let workouts = Object.values(this.props.workouts);
+
+        workouts.forEach( workout => {
+            if (workout.user_id === userId) {
+                this.userWorkouts.push(workout);
+            }
+        });
     }
 
     filterWorkoutsByType(workoutType) {
-        const typeWorkout = this.userWorkouts.filter((workout) => {return workout.workout_type === workoutType});
+        const typeWorkout = this.userWorkouts.filter((workout) => workout.workout_type === workoutType);
         switch (workoutType) {
             case ("run"):
-                this.userRuns = typeWorkout;
+                this.userRuns["workouts"] = typeWorkout;
+                this.userRuns["duration"] = this.totalDuration(typeWorkout);
+                this.userRuns["distance"] = this.totalDistance(typeWorkout);
+                this.userRuns["speed"] = (this.userRuns.distance / this.userRuns.duration)
                 break;
             case ("walk"):
-                this.userWalks = typeWorkout;
+                this.userWalks["workouts"] = typeWorkout;
+                this.userWalks["duration"] = this.totalDuration(typeWorkout);
+                this.userWalks["distance"] = this.totalDistance(typeWorkout);
+                this.userWalks["speed"] = (this.userWalks.distance / this.userWalks.duration)
                 break;
             case ("hike"):
-                this.userHikes = typeWorkout;
+                this.userHikes["workouts"] = typeWorkout;
+                this.userHikes["duration"] = this.totalDuration(typeWorkout);
+                this.userHikes["distance"] = this.totalDistance(typeWorkout);
+                this.userHikes["speed"] = (this.userHikes.distance / this.userHikes.duration)
                 break;
             case ("cycle"):
-                this.userCycles = typeWorkout;
+                this.userCycles["workouts"] = typeWorkout;
+                this.userCycles["duration"] = this.totalDuration(typeWorkout);
+                this.userCycles["distance"] = this.totalDistance(typeWorkout);
+                this.userCycles["speed"] = (this.userCycles.distance / this.userCycles.duration)
                 break;
             default:
                 break;
         }
     }
 
-    getUserDurations(type) {
-        
+    totalDuration(workouts) {
+        let duration = 0;
+        workouts.forEach( workout => {duration += workout.duration})
+        return duration;
     }
 
-    calculateUserMiles() {
-
+    totalDistance(workouts) {
+        let distance = 0;
+        workouts.forEach( workout => {distance += this.props.routes[workout.route_id].distance})
+        return distance;
     }
 
-    calculateUserAverageSpeed() {
-        
+    calculateStats() {
+        this.filterUserWorkouts(this.props.session.id);
+        this.workoutTypes.forEach( workoutType => {this.filterWorkoutsByType(workoutType)});
+        // create userStats function here
     }
 
     render() {
         if (Object.keys(this.props.routes).length < 1 && Object.keys(this.props.workouts).length < 1) { return null; }
+        (this.userWorkouts.length > 0) ? this.userWorkouts : this.calculateStats()
+        let userWorkouts = this.userWorkouts;
+        
+        
+        debugger
         return (
             <div>
                 <div>
-                    THIS WILL BE THE USER STATS
+                    <div></div>
+                    {userWorkouts.map( workout => {
+                        return <div key={Math.random()}>{`${workout.user_id}: ${workout.duration}`}</div>
+                    })}
                 </div>
             </div>
         )
