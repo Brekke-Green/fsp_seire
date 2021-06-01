52e901089a783bba4671cf621e25284c87a8b09e
Brekke Green <brekke.andrew.green@gmail.com>
2021-06-01 15:41:56 -0400

    Add personal links to dashboard page

--- frontend/components/seire/career_info.jsx
+++ frontend/components/seire/career_info.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const CareerInfo = () => (
-    <h1>MY CONTACT INFO WILL GO HERE</h1>
+    <div>
+        <div className="footer-col-label">FOLLOW</div>
+        <ul>
+            <li><a href="https://github.com/Brekke-Green">GITHUB</a></li>
+            <li><a href="https://www.linkedin.com/in/brekke-andrew-green/">LINKEDIN</a></li>
+            <li><a href="https://angel.co/u/brekke-andrew-green">ANGELLIST</a></li>
+            <li><a href="https://www.kaggle.com/brekkeandrewgreen">KAGGLE</a></li>
+        </ul>        
+    </div>
 )
 
 export default CareerInfo;
