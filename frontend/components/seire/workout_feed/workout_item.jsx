7c60d8cb0fd4448b2a5886f79eb756565604a78a
Brekke Green <brekke.andrew.green@gmail.com>
2021-06-01 15:42:42 -0400

    Create detailed workout feed with distance, time, pace and workout type

--- frontend/components/seire/workout_feed/workout_item.jsx
+++ frontend/components/seire/workout_feed/workout_item.jsx
@@ -3,11 +3,27 @@ import React from 'react';
 class WorkoutItem extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleNumbers = this.handleNumbers.bind(this);
+        this.handleTime = this.handleTime.bind(this);
     }
 
     componentDidMount() {
         this.props.getRoutes();
         this.props.getWorkouts();
+    }
+
+    handleNumbers(number) {
+        if (isNaN(number)) {return 0};
+        if (number % 2 === 0) {return number};
+        return Number.parseFloat(number).toFixed(2);
+    }
+
+    handleTime(time) {
+        const hours = Math.floor(time / 3600);
+        const minutes = Math.floor((time % 3600) / 60);
+        const seconds = ((time % 3600) % 60);
+        return `${hours}h ${minutes}m ${seconds}s`
     }
 
     render() {
@@ -17,7 +33,24 @@ class WorkoutItem extends React.Component {
         return (
             <div>
                 {Object.values(workouts).map(workout => (
-                    <div key={workout.id * 10}>{`${routes[workout.route_id]['route_name']}: ${routes[workout.route_id]['distance']/workout.duration}m/s`}</div>
+                    <div key={workout.id * 10}>
+                        <label>{`${routes[workout.route_id]['route_name']} -`}</label>
+                            <ul>
+                                <li>
+                                    {`Type: ${workout.workout_type.toUpperCase()}`}
+                                </li>
+                                <li>
+                                    {`Distance: ${this.handleNumbers(routes[workout.route_id]['distance'] / 1000)}km`}
+                                </li>
+                                <li>
+                                    {`Time: ${this.handleTime(workout.duration)}`}
+                                </li>
+                                <li>
+                                    {`Pace: ${this.handleNumbers(routes[workout.route_id]['distance']/workout.duration)}m/s`}
+                                </li>
+                                <br />
+                            </ul>  
+                    </div>
                 ))}
             </div>
         )
