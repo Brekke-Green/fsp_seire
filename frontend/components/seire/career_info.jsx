61b0dd9a6f7b6d10a80e96bc84265701c9fdfef5
Brekke Green <brekke.andrew.green@gmail.com>
2021-06-09 11:14:21 -0400

    Style dashboard and add personal links icons

--- frontend/components/seire/career_info.jsx
+++ frontend/components/seire/career_info.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+
+
+
+
+
 const CareerInfo = () => (
     <div id="personal-links-section">
         <div className="personal-links-container">
-            <div className="personal-links-title">CONTACT ME</div>
+            <div className="dashboard-title">CONTACT ME</div>
             <ul>
-                <li><a href="https://github.com/Brekke-Green">GITHUB</a></li>
-                <li><a href="https://www.linkedin.com/in/brekke-andrew-green/">LINKEDIN</a></li>
-                <li><a href="https://angel.co/u/brekke-andrew-green">ANGELLIST</a></li>
-                <li><a href="https://www.kaggle.com/brekkeandrewgreen">KAGGLE</a></li>
+                <li><i className="fas fa-user-astronaut"></i><a href="https://brekke-green.github.io/" target="_blank" rel="noopener noreferrer" className="personal-link-item">PERSONAL WEBSITE</a></li>
+                <li><i className="fab fa-linkedin-in"></i><a href="https://www.linkedin.com/in/brekke-andrew-green/" target="_blank" rel="noopener noreferrer" className="personal-link-item">LINKEDIN</a></li>
+                <li><i className="fab fa-github"></i><a href="https://github.com/Brekke-Green" target="_blank" rel="noopener noreferrer" className="personal-link-item">GITHUB</a></li>
+                <li><i className="fab fa-angellist"></i><a href="https://angel.co/u/brekke-andrew-green" target="_blank" rel="noopener noreferrer" className="personal-link-item">ANGELLIST</a></li>
             </ul>        
         </div>
     </div>
