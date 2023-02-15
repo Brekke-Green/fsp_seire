e84872413527b3d1ba678ec20ca6c080b70f3969
Brekke Green <brekke.andrew.green@gmail.com>
2023-02-15 18:38:18 -0500

    Update build script to include npm start

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+npm start &&
+bundle install &&
+bundle exec rake assets:precompile &&
+bundle exec rake assets:clean &&
+bundle exec rake db:migrate 
